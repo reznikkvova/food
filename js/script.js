db7295d7c22c1ca4359954f78fbedfaa3c08bc9d
Vova_Reznik <reznikvova2222@gmail.com>
2021-01-25 23:47:05 +0200

    timer update

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ testWebP(function (support) {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
-
 window.addEventListener('DOMContentLoaded', () => {
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
@@ -52,4 +51,59 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+});
+window.addEventListener('DOMContentLoaded', () => {
+
+const deadline = '2021-03-25';
+
+function getTimeRemaining(endtime) {
+	const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t/(1000*60*60*24)),
+			hours = Math.floor((t / (1000*60*60)) % 24),
+			minutes = Math.floor((t / (1000*60)) % 60 ),
+			seconds = Math.floor((t/1000) % 60);
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function getZero(num) {
+	if(num >= 0 && num < 10) {
+		return `0${num}`;
+	}
+	else {
+		return num;
+	}
+}
+
+function setClock(selector, endtime) {
+	const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInerval = setInterval(updateClock, 1000);
+
+	updateClock();
+
+	function updateClock() {
+		const t = getTimeRemaining(endtime);
+
+		days.innerHTML = getZero(t.days);
+		hours.innerHTML = getZero(t.hours);
+		minutes.innerHTML = getZero(t.minutes);
+		seconds.innerHTML = getZero(t.seconds);
+
+		if (t.total <=0) {
+			clearInterval(timeInerval);
+		}
+	}
+}
+
+setClock('.timer', deadline);
+
 });
